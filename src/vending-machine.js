08cc4256159d54d4f87156c91c5e119bbddbeb29
Utsab Bera <utsab@Utsabs-MacBook-Pro.local>
2023-04-11 17:06:42 +0530

    upgraded functionality to dispense 5 rupee coins alongside

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,5 +1,15 @@
+const determineFiveRupeeCoinsToDispense = function(amount) {
+  return Math.floor(amount / 5);
+}
+
 const determineNoOfCoinsToDispense = function(amount) {
-  return Math.floor(amount / 2) + (amount % 2)
+  const fiveRupeeCoins =  determineFiveRupeeCoinsToDispense(amount)
+  let remaining = amount % 5;
+  const twoRupeeCoins = Math.floor(remaining / 2);
+  remaining = remaining % 2;
+  const oneRupeeCoins = remaining;
+
+  return fiveRupeeCoins + twoRupeeCoins + oneRupeeCoins;
 }
 
 exports.determineNoOfCoinsToDispense = determineNoOfCoinsToDispense;
--- tests/vending-machine-tests.js
+++ tests/vending-machine-tests.js
@@ -10,12 +10,15 @@ assert("determineNoOfCoinsToDispense",
   "amount 0 shoud give 0 coins");
 assert("determineNoOfCoinsToDispense", 
   vendingMachine.determineNoOfCoinsToDispense(1), 1, 
-  "amount 1 shoud give 1 coin");
+  "amount 1 shoud give 1 coin (1:1)");
 assert("determineNoOfCoinsToDispense", 
   vendingMachine.determineNoOfCoinsToDispense(3), 2, 
-  "amount 3 shoud give 2 coin");
+  "amount 3 shoud give 2 coin (2:1, 1:1)");
 assert("determineNoOfCoinsToDispense", 
-  vendingMachine.determineNoOfCoinsToDispense(5), 3, 
-  "amount 5 shoud give 3 coins");
+  vendingMachine.determineNoOfCoinsToDispense(5), 1, 
+  "amount 5 shoud give 1 coins (5:1)");
+assert("determineNoOfCoinsToDispense", 
+  vendingMachine.determineNoOfCoinsToDispense(7), 2, 
+  "amount 5 shoud give 1 coins (5:1, 2:1)");
 
 console.log(getReport());
