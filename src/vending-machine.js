70ca2f28162435876a4f1a52bd65008d7931ab44
Utsab Bera <utsab@Utsabs-MacBook-Pro.local>
2023-04-11 17:15:31 +0530

    seperated coin calculation functionality of a denomination returning coins and extra as object

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,13 +1,14 @@
-const determineFiveRupeeCoinsToDispense = function(amount) {
-  return Math.floor(amount / 5);
+const determineNoOfCoinsToDispenseOf = function(amount, denomination) {
+  return {coins : Math.floor(amount / denomination), 
+    extra : amount % denomination};
 }
 
 const determineNoOfCoinsToDispense = function(amount) {
-  const fiveRupeeCoins =  determineFiveRupeeCoinsToDispense(amount)
-  let remaining = amount % 5;
-  const twoRupeeCoins = Math.floor(remaining / 2);
-  remaining = remaining % 2;
-  const oneRupeeCoins = remaining;
+  const fiveRupeeCoins =  determineNoOfCoinsToDispenseOf(amount, 5).coins;
+  let remaining = determineNoOfCoinsToDispenseOf(amount, 5).extra;
+  const twoRupeeCoins = determineNoOfCoinsToDispenseOf(remaining, 2).coins;
+  remaining = determineNoOfCoinsToDispenseOf(remaining, 2).extra;
+  const oneRupeeCoins = determineNoOfCoinsToDispenseOf(remaining, 1).coins;
 
   return fiveRupeeCoins + twoRupeeCoins + oneRupeeCoins;
 }
