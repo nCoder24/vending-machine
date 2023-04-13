43c0e16fd2260ce82792b0bff9c924a0a73ab609
Utsab Bera <nCoder24@users.noreply.github.com>
2023-04-13 11:18:36 +0530

    added bubble sorting mechanism in array-utils

--- tests/array-utils-tests.js
+++ tests/array-utils-tests.js
@@ -1,23 +1,30 @@
 const test = require("../lib/testing-utilities.js");
-const vendingMachine = require("../lib/array-utils.js");
+const array = require("../lib/array-utils.js");
 
 const assert = test.assertTest;
 const assertArray = test.assertTestOnArray;
 const getReport = test.generateAssertionReport;
 
 const assertSorting = function() {
-  assertArray("maxSort", vendingMachine.maxSort([1, 2, 3]), [3, 2, 1], "sould be in assending order"); 
-  assertArray("maxSort", vendingMachine.maxSort([2, 1, 3]), [3, 2, 1], "sould be in assending order"); 
-  assertArray("maxSort", vendingMachine.maxSort([]), [], "sould be empty");
+  assertArray("maxSort", array.maxSort([1, 2, 3]), [3, 2, 1], "sould be in assending order"); 
+  assertArray("maxSort", array.maxSort([2, 1, 3]), [3, 2, 1], "sould be in assending order"); 
+  assertArray("maxSort", array.maxSort([]), [], "sould be empty");
+}
+
+const assertBubbleSort = function() {
+  assertArray("bubbleSort", array.bubbleSort([1, 2, 3]), [3, 2, 1], "sould be in assending order"); 
+  assertArray("bubbleSort", array.bubbleSort([2, 1, 3]), [3, 2, 1], "sould be in assending order"); 
+  assertArray("bubbleSort", array.bubbleSort([]), [], "sould be empty");
 }
 
 const assertMax = function() {
-  assert("max", vendingMachine.max([1, 2, 3]), 3, "sould be in assending order"); 
-  assert("max", vendingMachine.max([2, 1, 3]),  3, "sould be in assending order"); 
-  assert("max", vendingMachine.max([2]),  2, "sould be in assending order"); 
+  assert("max", array.max([1, 2, 3]), 3, "last element should be max"); 
+  assert("max", array.max([2, 4, 3]),  4, "middle element should be largest"); 
+  assert("max", array.max([2]),  2, "only element should be max"); 
 }
 
 assertSorting();
 assertMax();
+assertBubbleSort();
 
-//console.log(getReport());
+console.log(getReport());
